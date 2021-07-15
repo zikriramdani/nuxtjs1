1eea4f35586f94f10747c40cbccace1c8a186207
ZR <zikriramdani.developer@gmail.com>
2021-07-16 02:50:03 +0700

    M

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -2,39 +2,111 @@
   <v-row justify="center" align="center">
     <v-col cols="12">
       <v-dialog
-        v-model="dialog"
-        width="500"
+        v-model="dialogTambah"
+        persistent
+        max-width="600px"
       >
         <template #activator="{ on, attrs }">
           <v-btn
-            color="accent lighten-2"
+            color="primary"
             dark
             v-bind="attrs"
             v-on="on"
           >
-            Tambah books
+            Tambah Books
           </v-btn>
         </template>
-
         <v-card>
-          <v-card-title class="text-h5 grey lighten-2">
-            Privacy Policy
+          <v-card-title>
+            <span class="text-h5">Tambah Books</span>
           </v-card-title>
-
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <v-container>
+              <v-row>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Legal first name*"
+                    required
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Legal middle name"
+                    hint="example of helper text only on focus"
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Legal last name*"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                  />
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Email*"
+                    required
+                  />
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Password*"
+                    type="password"
+                    required
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
+                  <v-select
+                    :items="['0-17', '18-29', '30-54', '54+']"
+                    label="Age*"
+                    required
+                  />
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                >
+                  <v-autocomplete
+                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                    label="Interests"
+                    multiple
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
           </v-card-text>
-
-          <v-divider />
-
           <v-card-actions>
             <v-spacer />
             <v-btn
-              color="primary"
+              color="blue darken-1"
               text
-              @click="dialog = false"
+              @click="dialogTambah = false"
             >
-              I accept
+              Close
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="dialogTambah = false"
+            >
+              Save
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -60,19 +132,228 @@
             </v-card-subtitle>
 
             <v-card-actions>
-              <v-btn
-                color="orange"
-                text
+              <v-dialog
+                v-model="dialogDetails"
+                persistent
+                max-width="600px"
               >
-                Details
-              </v-btn>
+                <template #activator="{ on, attrs }">
+                  <v-btn
+                    color="orange"
+                    text
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    Details
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">Details Books</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            label="Legal first name*"
+                            required
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            label="Legal middle name"
+                            hint="example of helper text only on focus"
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            label="Legal last name*"
+                            hint="example of persistent helper text"
+                            persistent-hint
+                            required
+                          />
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field
+                            label="Email*"
+                            required
+                          />
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field
+                            label="Password*"
+                            type="password"
+                            required
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                        >
+                          <v-select
+                            :items="['0-17', '18-29', '30-54', '54+']"
+                            label="Age*"
+                            required
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                        >
+                          <v-autocomplete
+                            :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                            label="Interests"
+                            multiple
+                          />
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                    <small>*indicates required field</small>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer />
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="dialogDetails = false"
+                    >
+                      Close
+                    </v-btn>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="dialogDetails = false"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
 
-              <v-btn
-                color="orange"
-                text
+              <!-- Edit Data -->
+              <v-dialog
+                v-model="dialogEdit"
+                persistent
+                max-width="600px"
               >
-                Edit
-              </v-btn>
+                <template #activator="{ on, attrs }">
+                  <v-btn
+                    color="orange"
+                    text
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    Edit
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">Edit Books</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            label="Legal first name*"
+                            required
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            label="Legal middle name"
+                            hint="example of helper text only on focus"
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            label="Legal last name*"
+                            hint="example of persistent helper text"
+                            persistent-hint
+                            required
+                          />
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field
+                            label="Email*"
+                            required
+                          />
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field
+                            label="Password*"
+                            type="password"
+                            required
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                        >
+                          <v-select
+                            :items="['0-17', '18-29', '30-54', '54+']"
+                            label="Age*"
+                            required
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                        >
+                          <v-autocomplete
+                            :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                            label="Interests"
+                            multiple
+                          />
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                    <small>*indicates required field</small>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer />
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="dialogEdit = false"
+                    >
+                      Close
+                    </v-btn>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="dialogEdit = false"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -85,7 +366,11 @@
 export default {
 	data () {
 		return {
-			dialog: false,
+			dialogTambah: false,
+
+			dialogDetails: false,
+
+			dialogEdit: false,
 
 			loading: false,
       		selection: 1,
